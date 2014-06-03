6b5b13fb1eddd8dd4610e58ec2c1e4522f4efa8d
Cid <cid@livefyre.com>
2014-06-03 11:09:57 -0400

    consistant quots in bootstrap.js

--- models/bootstrap.js
+++ models/bootstrap.js
@@ -1,44 +1,45 @@
-var Constants = require('../models/constants');
+'use strict';
 
+var Constants = require('../models/constants');
 var Bootstrap = {
-  "comments": [{
-    "articleId" : Constants.DEMO_ARTICLE_ID_PREFIX + "_0001",
-    "title" : "Comments",
-    "url" : Constants.DEMO_URL_PREFIX + "comments"
+  'comments': [{
+    'articleId' : Constants.DEMO_ARTICLE_ID_PREFIX + '_0001',
+    'title' : 'Comments',
+    'url' : Constants.DEMO_URL_PREFIX + 'comments'
    }],
-   "liveblog": [{
-    "articleId" : Constants.DEMO_ARTICLE_ID_PREFIX + "_0002",
-    "title" : "Live Blog",
-    "url" : Constants.DEMO_URL_PREFIX + "liveblog",
-    "type": "liveblog"
+   'liveblog': [{
+    'articleId' : Constants.DEMO_ARTICLE_ID_PREFIX + '_0002',
+    'title' : 'Live Blog',
+    'url' : Constants.DEMO_URL_PREFIX + 'liveblog',
+    'type': 'liveblog'
    }],
-   "livechat": [{
-    "articleId": Constants.DEMO_ARTICLE_ID_PREFIX + "_0003",
-    "title":"Live Chat",
-    "url": Constants.DEMO_URL_PREFIX + "livechat",
-    "type":"livechat"
+   'livechat': [{
+    'articleId': Constants.DEMO_ARTICLE_ID_PREFIX + '_0003',
+    'title':'Live Chat',
+    'url': Constants.DEMO_URL_PREFIX + 'livechat',
+    'type':'livechat'
    }],
-   "livereviews": [{
-    "articleId": Constants.DEMO_ARTICLE_ID_PREFIX + "_0004",
-    "title": "Live Reviews",
-    "url": Constants.DEMO_URL_PREFIX + "livereviews",
-    "type":"reviews"
+   'livereviews': [{
+    'articleId': Constants.DEMO_ARTICLE_ID_PREFIX + '_0004',
+    'title': 'Live Reviews',
+    'url': Constants.DEMO_URL_PREFIX + 'livereviews',
+    'type':'reviews'
    }],
-   "sidenotes": [{
-    "articleId": Constants.DEMO_ARTICLE_ID_PREFIX + "_sidenotes_0001",
-    "title": "Sidenotes",
-    "url": Constants.DEMO_URL_PREFIX + "sidenotes",
-    "type":"sidenotes"
+   'sidenotes': [{
+    'articleId': Constants.DEMO_ARTICLE_ID_PREFIX + '_sidenotes_0001',
+    'title': 'Sidenotes',
+    'url': Constants.DEMO_URL_PREFIX + 'sidenotes',
+    'type':'sidenotes'
    }],
-   "mediawall": [{
-    "articleId":"custom-1397754619766",
-    "title":"Media Wall",
-    "url": Constants.DEMO_URL_PREFIX + "mediawall"
+   'mediawall': [{
+    'articleId':'custom-1397754619766',
+    'title':'Media Wall',
+    'url': Constants.DEMO_URL_PREFIX + 'mediawall'
    }],
-   "gallery": [{
-    "articleId":"custom-1395952392273",
-    "title":"Streamhub Gallery",
-    "url": Constants.DEMO_URL_PREFIX + "gallery"
+   'gallery': [{
+    'articleId':'custom-1395952392273',
+    'title':'Streamhub Gallery',
+    'url': Constants.DEMO_URL_PREFIX + 'gallery'
    }]
 };
 
--- routes/mediawall.js
+++ routes/mediawall.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var express = require('express');
 var router = express.Router();
 var Constants = require('../models/constants');
@@ -5,27 +7,32 @@ var Bootstrap = require('../models/bootstrap');
 var Content = require('../models/content');
 
 var articles = [];
+var articleType = articleType || {};
+
 for (articleType in Bootstrap) {
-    articles.push(Bootstrap[articleType][0]);
+  articles.push(Bootstrap[articleType][0]);
 }
 
 /** comments page */
 router.get('/:index(\\d+)?', function(req, res) {
-    if (typeof req.params.index == 'undefined') {
+
+    if (typeof req.params.index === 'undefined') {
         req.params.index = 0; // default to zeroith
-    };
+    }
+
     var article = Bootstrap.comments[req.params.index];
     var content = new Content.Content();
-    
+
     var meta = content.buildCollectionMeta(article.title, article.articleId, article.url, [], []);
-        
-    res.render('mediawall', { pagetitle: 'Media Wall', 
-        Constants: Constants, 
+
+    res.render('mediawall', { pagetitle: 'Media Wall',
+        Constants: Constants,
         Bootstrap: Bootstrap,
         articles: articles,
         article: article,
         meta: meta
     });
+
 });
 
 module.exports = router;
