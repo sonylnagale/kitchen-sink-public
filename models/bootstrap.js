937b03d7631214d3d237029d91d7e4c87c429f3e
Cid <cid@livefyre.com>
2014-06-03 09:45:05 -0400

    DEMO_URL_PREFIX is no longer local... and is just home so can run on any local url/port

--- models/bootstrap.js
+++ models/bootstrap.js
@@ -1,44 +1,45 @@
 var Constants = require('../models/constants');
+
 var Bootstrap = {
-	"comments": [{
-		"articleId" : Constants.DEMO_ARTICLE_ID_PREFIX + "_0001",
-		"title" : "Comments",
-		"url" : "http://" + Constants.DEMO_URL_PREFIX + "/comments"
-	 }],
-	 "liveblog": [{
-	 	"articleId" : Constants.DEMO_ARTICLE_ID_PREFIX + "_0002",
-	 	"title" : "Live Blog",
-	 	"url" : "http://" + Constants.DEMO_URL_PREFIX + "/liveblog",
-	 	"type": "liveblog"
-	 }],
-	 "livechat": [{
-	 	"articleId": Constants.DEMO_ARTICLE_ID_PREFIX + "_0003",
-	 	"title":"Live Chat",
-	 	"url":"http://" + Constants.DEMO_URL_PREFIX + "/livechat",
-	 	"type":"livechat"
-	 }],
-	 "livereviews": [{
-	 	"articleId": Constants.DEMO_ARTICLE_ID_PREFIX + "_0004",
-	 	"title": "Live Reviews",
-	 	"url":"http://" + Constants.DEMO_URL_PREFIX + "/livereviews",
-	 	"type":"reviews"
-	 }],
-	 "sidenotes": [{
-	 	"articleId": Constants.DEMO_ARTICLE_ID_PREFIX + "_sidenotes_0001",
-	 	"title": "Sidenotes",
-	 	"url": "http://" + Constants.DEMO_URL_PREFIX + "/sidenotes",
-	 	"type":"sidenotes"
-	 }],
-	 "mediawall": [{
-	 	"articleId":"custom-1397754619766",
-	 	"title":"Media Wall",
-	 	"url":"http://" + Constants.DEMO_URL_PREFIX + "/mediawall"
-	 }],
-	 "gallery": [{
-	 	"articleId":"custom-1395952392273",
-	 	"title":"Streamhub Gallery",
-	 	"url":"http://" + Constants.DEMO_URL_PREFIX + "/gallery"
-	 }]
+  "comments": [{
+    "articleId" : Constants.DEMO_ARTICLE_ID_PREFIX + "_0001",
+    "title" : "Comments",
+    "url" : Constants.DEMO_URL_PREFIX + "comments"
+   }],
+   "liveblog": [{
+    "articleId" : Constants.DEMO_ARTICLE_ID_PREFIX + "_0002",
+    "title" : "Live Blog",
+    "url" : Constants.DEMO_URL_PREFIX + "liveblog",
+    "type": "liveblog"
+   }],
+   "livechat": [{
+    "articleId": Constants.DEMO_ARTICLE_ID_PREFIX + "_0003",
+    "title":"Live Chat",
+    "url": Constants.DEMO_URL_PREFIX + "livechat",
+    "type":"livechat"
+   }],
+   "livereviews": [{
+    "articleId": Constants.DEMO_ARTICLE_ID_PREFIX + "_0004",
+    "title": "Live Reviews",
+    "url": Constants.DEMO_URL_PREFIX + "livereviews",
+    "type":"reviews"
+   }],
+   "sidenotes": [{
+    "articleId": Constants.DEMO_ARTICLE_ID_PREFIX + "_sidenotes_0001",
+    "title": "Sidenotes",
+    "url": Constants.DEMO_URL_PREFIX + "sidenotes",
+    "type":"sidenotes"
+   }],
+   "mediawall": [{
+    "articleId":"custom-1397754619766",
+    "title":"Media Wall",
+    "url": Constants.DEMO_URL_PREFIX + "mediawall"
+   }],
+   "gallery": [{
+    "articleId":"custom-1395952392273",
+    "title":"Streamhub Gallery",
+    "url": Constants.DEMO_URL_PREFIX + "gallery"
+   }]
 };
 
 module.exports = Bootstrap;
--- models/constants.js
+++ models/constants.js
@@ -8,7 +8,7 @@ var Constants = {
 		"LIVEFYRE_SITE_URL": 			"http://local.livefyre.com/",
 		"LIVEFYRE_SYSTEM_USER_TOKEN": 	"eyJhbGciOiAiSFMyNTYiLCAidHlwIjogIkpXVCJ9.eyJkb21haW4iOiAiY2xpZW50LXNvbHV0aW9ucy11YXQuZnlyZS5jbyIsICJleHBpcmVzIjogMTM5ODUzNDgzMy41NjUzNDIsICJ1c2VyX2lkIjogInN5c3RlbSJ9.1QMfgW65-GX4qchXHuGScAr9PEbzQfSgWBbKh83axg0",
 		"DEMO_ARTICLE_ID_PREFIX":		"dark-rough-breeze",
-		"DEMO_URL_PREFIX":				"local.livefyre.com"
+		"DEMO_URL_PREFIX":				"/"
 
 };
 
