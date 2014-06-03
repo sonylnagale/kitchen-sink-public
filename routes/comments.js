7b87a7a0188e6de5edd65203257802390104d08c
sonylnagale <sonyl@livefyre.com>
2014-06-03 16:31:44 -0400

    Cleanup

--- routes/comments.js
+++ routes/comments.js
@@ -32,22 +32,6 @@ router.get('/:index(\\d+)?', function(req, res) {
 			meta: meta
 		});
 	});
-
-    if (typeof req.params.index == 'undefined') {
-        req.params.index = 0; // default to zeroith
-    };
-    var article = Bootstrap.comments[req.params.index];
-    var content = new Content.Content();
-    
-    var meta = content.buildCollectionMeta(article.title, article.articleId, article.url, [], []);
-        
-    res.render('comments', { pagetitle: 'Comments', 
-        Constants: Constants, 
-        Bootstrap: Bootstrap,
-        articles: articles,
-        article: article,
-        meta: meta
-    });
 });
 
 module.exports = router;
