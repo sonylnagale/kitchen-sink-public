bf5290d1bc57a961783df6664d0fc2b73619322e
sonylnagale <sonyl@livefyre.com>
2014-06-06 18:42:27 -0400

    Using crypt

--- make.js
+++ make.js
@@ -1,6 +1,3 @@
-var gpg = require('gpg');
-var fs = require('fs');
-
 function ask(question, callback) {
  var stdin = process.stdin, stdout = process.stdout;
  
@@ -9,29 +6,45 @@ function ask(question, callback) {
  
  stdin.once('data', function(data) {
    data = data.toString().trim();
-   stdout.write("OK, type " + data + " in the next window.\n");
-
    callback(data);
    
  });
-}
-
-
-
+};
 
 ask("Who has the biggest office in Livefyre? ", function (keyphrase) {
-	gpg.decryptFile('./models/constants.js.gpg', function(err, contents){
-			if (err) {
-				console.log(err);
-			} else {
-				fs.writeFile("models/constants.js", contents, function(err) {
-				    if(err) {
-				        console.log(err);
-				    } else {
-				        console.log("Ready to go!");
-				        process.exit();
-				    }
-				}); 
-			}
-	});
+	var crypto = require('crypto');
+
+	function md5(data) {
+	    var hash = crypto.createHash('md5');
+	    hash.update(data);
+	    return new Buffer(hash.digest('hex'), 'hex');
+	}
+
+	var text = require('fs').readFileSync('./models/constants.js.crypt');
+	var salt = text.slice(8, 16);
+	var cryptotext = text.slice(16);
+	var password = new Buffer(keyphrase);
+
+	var hash0 = new Buffer('');
+	var hash1 = md5(Buffer.concat([ hash0, password, salt ]));
+	var hash2 = md5(Buffer.concat([ hash1, password, salt ]));
+	var hash3 = md5(Buffer.concat([ hash2, password, salt ]));
+	var key = Buffer.concat([ hash1, hash2 ]);
+	var iv = hash3;
+
+	var decoder = crypto.createDecipheriv('aes-256-cbc', key, iv);
+
+	var chunks = [];
+	chunks.push(decoder.update(cryptotext, "binary", "utf8"));
+	chunks.push(decoder.final("utf8"));
+	
+	var fs = require('fs');
+	fs.writeFile("./models/constants.js", chunks.join(''), function(err) {
+	    if(err) {
+	        console.log(err);
+	    } else {
+	        console.log("Done!");
+	        process.exit();
+	    }
+	}); 
 });
